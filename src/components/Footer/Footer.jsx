95b745fea9cda6ad408c2d06a14d496671f92598
Vishesh <ranavisheshsingh242@gmail.com>
2023-11-26 13:20:42 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+
+const quickLinks = [
+  {
+    path: "/about",
+    display: "About",
+  },
+
+  {
+    path: "#",
+    display: "Privacy Policy",
+  },
+
+  {
+    path: "/cars",
+    display: "Car Listing",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo footer__logo">
+              <h1>
+                <Link to="/home" className=" d-flex align-items-center gap-2">
+                  <i class="ri-car-line"></i>
+                  <span>
+                    StarExpo <br /> 
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+            "Welcome to StarExpo, your go-to destination for hassle-free car rentals! Whether you're planning a weekend getaway, a business trip, or need a temporary vehicle replacement, we've got you covered. Our user-friendly website offers a wide range of vehicles to suit every need and budget.
+            </p>
+          </Col>
+
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Head Office</h5>
+              <p className="office__info">New York City, America</p>
+              <p className="office__info">Phone: +0995345875365</p>
+
+              <p className="office__info">Email: carServe1223@gmail.com</p>
+
+              <p className="office__info">Office Time: 10am - 7pm</p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="12">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">Subscribe our newsletter</p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>Copyright {year}, Developed by
+                Star Expo. All rights reserved.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
